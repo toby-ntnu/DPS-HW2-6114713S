81bd19c8eee72759d5408f0c92ba3632e81a82f3
toby-ntnu <136100214+toby-ntnu@users.noreply.github.com>
2023-06-09 23:03:40 +0800

    Create 2.py

---
+++ 2.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import numpy as np
+import matplotlib as mb
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+df6 = pd.read_csv('population_density_106.csv')
+df7 = pd.read_csv('population_density_107.csv')
+df8 = pd.read_csv('population_density_108.csv')
+
+x1 = np.arange(1, 67, 1)
+df106 = df6.groupby("county")["people_total"].sum()
+df107 = df7.groupby("county")["people_total"].sum()
+df108 = df8.groupby("county")["people_total"].sum()
+
+x = x1[:,np.newaxis]
+y1 = df106[:,np.newaxis]
+y2 = df107[:,np.newaxis]
+y3 = df108[:,np.newaxis]
