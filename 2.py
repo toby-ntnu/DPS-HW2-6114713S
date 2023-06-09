03fa493658d8793c3482736616d9627b56790e61
toby-ntnu <136100214+toby-ntnu@users.noreply.github.com>
2023-06-09 23:04:03 +0800

    Update 2.py

--- 2.py
+++ 2.py
@@ -6,8 +6,7 @@ from sklearn.cluster import KMeans
 df6 = pd.read_csv('population_density_106.csv')
 df7 = pd.read_csv('population_density_107.csv')
 df8 = pd.read_csv('population_density_108.csv')
-
-x1 = np.arange(1, 67, 1)
+x1 = np.arange(1, 67, 1)
 df106 = df6.groupby("county")["people_total"].sum()
 df107 = df7.groupby("county")["people_total"].sum()
 df108 = df8.groupby("county")["people_total"].sum()
@@ -16,3 +15,7 @@ x = x1[:,np.newaxis]
 y1 = df106[:,np.newaxis]
 y2 = df107[:,np.newaxis]
 y3 = df108[:,np.newaxis]
+#print(x,"y=",y)
+y = np.vstack((y1,y2,y3))
+A = np.hstack((x,y))
+#B = np.hstack()
